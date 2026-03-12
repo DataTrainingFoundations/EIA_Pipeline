cdf8b8efafea43428a5356d4b3d69266401f59bd
ericsinkler <ericsinkler2543@gmail.com>
2026-03-11 22:17:21 -0400

    Removed letter added by accident

--- airflow/dags/eia_pipeline.py
+++ airflow/dags/eia_pipeline.py
@@ -37,7 +37,7 @@ MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT", "http://minio:9000")
 MINIO_USER = os.environ.get("MINIO_ROOT_USER", "minioadmin")
 MINIO_PASSWORD = os.environ.get("MINIO_ROOT_PASSWORD", "minioadmin")
 
-POSTGRES_HOST = os.environ.get("POSTGRESd_HOST", "localhost")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.environ.get("POSTGRES_DB", "platform")
 POSTGRES_USER = os.environ.get("POSTGRES_USER", "platform")
